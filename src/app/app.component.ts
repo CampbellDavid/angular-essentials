d4e30dee9d5e53875c4314496ac2f8c1f66e22bf
CampbellDavid <dcampbell092@gmail.com>
2020-11-05 15:21:55 +0000

    add custom events listner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  aNumber = 2000;
+  rootName = 'David';
+  onNameChanged(newName) {
+    this.rootName = newName
+  }
 }
--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core'
+import { Component, EventEmitter, Input, Output } from '@angular/core'
 
 @Component({
   selector: 'app-user',
   template: `
-    <input type="text" [(ngModel)]="name">
+    <input type="text" (input)="onUserInput($event)" [value]="name">
+    <!-- <input type="text" [(ngModel)]="name"> -->
     <p>Hi there, {{ name }}</p>
     <p>I'm the user component</p>
   `
 })
 
 export class UserComponent {
-  name = 'David';
+  @Input() name;
+  @Output() nameChanged = new EventEmitter<string>()
   onUserInput(event) {
-    this.name = event.target.value
+    // this.name = event.target.value
+    this.nameChanged.emit(event.target.value)
   }
 }
