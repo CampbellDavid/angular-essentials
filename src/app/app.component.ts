8211e4dec13b7c89915bec6fd718e585b5710ac5
CampbellDavid <dcampbell092@gmail.com>
2020-11-06 11:21:47 +0000

    add lodash pkg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   rootItems = ['Apples', 'Bananas', 'Cherries']
   rootName = 'David';
+  title = 'app'
+  number = 1
   onNameChanged(newName) {
     this.rootName = newName
   }
   onItemWasAdded(newItem) {
     this.rootItems.push(newItem)
     console.log(this.rootItems)
+  }
+  onIncrease() {
+    this.number = this.number * 2
   }
 }
